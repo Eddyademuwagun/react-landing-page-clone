8774e620e5938e2aa20ee1ed5fb612b268c96f92
Eddyademuwagun <ademuwaguneddy@gmail.com>
2026-02-13 14:26:44 +0100

    Add button

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import cookiejar from '../images/Cookie.svg';
 import styles from './Content.module.scss';
 
 export default function Content() {
   return (
     <div className={styles.content}>
       <div className={styles.textBox}>
-        <p className={styles.text}>
-          <div>Dive into exciting </div>
-          <div>categories.</div>
-          <div>Discover new websites.</div>
-        </p>
+        <ul className={styles.list}>
+          <li>
+            <img src={cookiejar} alt="cookiejar" />
+          </li>
+          <li>
+            <p className={styles.text}>
+              <span>Dive into exciting </span>
+              <span>categories.</span>
+              <span>Discover new websites.</span>
+            </p>
+          </li>
+        </ul>
+        <button>Get Started</button>
+
         <img alt=""></img>
       </div>
     </div>
